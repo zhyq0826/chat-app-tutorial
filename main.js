43d6daac99a96e3ca3a82230fe8c9f4e6775bf67
zhaoyongqiang <zhyq0826@gmail.com>
2017-06-24 20:55:51 +0800

    finish js

--- main.js
+++ main.js
@@ -3,25 +3,35 @@ var path = require("path");
 var nunjucks = require('nunjucks');
 var uuidv4 = require('uuid/v4');
 var bodyParser = require('body-parser');
+var Promise = require('bluebird');
+var events = require('events');
 
+var messageEmitter = new events.EventEmitter();
+//设置最大监听数量
+messageEmitter.setMaxListeners(0)
 
 var messageBuffer = {
     waiters: [],
+    messageEmitter: messageEmitter
+};
+
+messageBuffer.newMessage = function(messages){
+    // for(var j = 0, length2 = this.waiters.length; j < length2; j++){
+    //     this.waiters[j].promise.set(messages);
+    // }
+    this.messageEmitter.emit('newMessage', messages);
 }
 
-messageBuffer.newMessage = function(){
-
+messageBuffer.newWaiter = function(client){
+    this.waiters.push(client);
 }
 
-messageBuffer.newWaiter = function(){
-
+function makeClient(id, promise){
+    return {
+        id: id,
+        promise: promise
+    }
 }
-
-
-function Client(){
-    var id, message = '', '';
-}
-
 
 var app = express();
 app.use('/static', express.static('static'));
@@ -38,18 +48,28 @@ app.get('/', function(req, res){
     res.render('index.html');
 });
 
-app.get('/a/message/new', function(req, res){
+app.post('/a/message/new', function(req, res){
     var message = {
         'id': uuidv4(),
         'body': req.body.body
     }
     message['html'] = '<div class="message" id="m'+message.id+'">'+message.body+'</div>';
     messageBuffer.newMessage([message])
+    res.end();
 });
 
-app.get('/a/message/updates', function(req, res){
-    
-    res.set('Content-Type', 'application/json');
+app.post('/a/message/updates', function(req, res){
+    var p = new Promise(function(resolve, reject){
+        messageBuffer.messageEmitter.on("newMessage", function(data){
+            resolve(data);
+        });
+    });
+    var client = makeClient(uuidv4(), p);
+    messageBuffer.newWaiter(client);
+    p.then(function(data){
+        res.set('Content-Type', 'application/json');
+        res.json({'messages': data});
+    });
 });
 
 var server = app.listen(3000, function(){
